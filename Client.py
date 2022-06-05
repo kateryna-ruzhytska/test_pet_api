5840ce8e6f45b78068d1b95ef699d4e8522d2501
Kateryna Ruzhytska <kate.ruzhitskaya@gmail.com>
2022-06-05 13:04:48 +0300

    update client

--- Client.py
+++ Client.py
@@ -1,9 +1,8 @@
 from PetManager import PetManager
 
-if __name__ == "__main__":
 
-    PetManager().find_pets_by_status("available")
-    PetManager().find_pets_by_status("sold")
-    PetManager().add_new_pet()
-    PetManager().find_pet_by_id(16)
-    PetManager().delete_pet(16)
+PetManager().find_pets_by_status("available")
+PetManager().find_pets_by_status("sold")
+PetManager().add_new_pet()
+PetManager().find_pet_by_id(16)
+PetManager().delete_pet(16)
